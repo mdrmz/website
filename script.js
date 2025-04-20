3c22ab89638b1f217f72b157befa5f2b97d177d6
mdrmz <95125171+mdrmz@users.noreply.github.com>
2025-04-20 15:34:36 +0300

    firsts

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Mesajınız gönderildi! Teşekkürler.");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
   });
-  
+});
+
+document.querySelectorAll('.animate-on-scroll').forEach(el => observer.observe(el));
